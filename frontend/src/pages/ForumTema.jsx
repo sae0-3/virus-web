45217a50718b79b99d2ec9b9a876b798617e6ec5
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-05-05 16:19:17 -0400

    .

--- frontend/src/pages/ForumTema.jsx
+++
@@ -1,9 +0,0 @@
-export const ForumTema = () => {
-  return (
-    <section>
-      <div className="container">
-        <h1>Te encuentras en el tema con id: {location.pathname.split('/')[2]}</h1>
-      </div>
-    </section>
-  )
-}
--- frontend/src/pages/NoMatch.jsx
+++
@@ -1,7 +0,0 @@
-export const NoMatch = () => {
-  return (
-    <div className="container">
-      Pagina no encontrada
-    </div>
-  )
-}
