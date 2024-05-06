b6a680b7090a6ac6642bce8cccad749c8b84f0f1
Daniel F. Merida Ojalvo <danmeroja@gmail.com>
2024-05-05 23:16:43 -0400

    version 0.1

--- backend/structure.sql
+++ backend/structure.sql
@@ -86,4 +86,97 @@ CREATE TABLE VISUALIZACION (
 
 
 
+-- INSERCIONES
+INSERT INTO USUARIO (username, correo_electronico, nombre, apellido_paterno, apellido_materno, numero_celular, foto_perfil)
+VALUES
+  ('usuario1', '2000000001@est.umss.edu', 'Juan', 'Pérez', 'González', '1234567890', 'https://randomuser.me/api/portraits/men/74.jpg'),
+  ('usuario2', '2000000002@est.umss.edu', 'María', 'García', 'López', '0987654321', 'https://randomuser.me/api/portraits/women/74.jpg'),
+  ('usuario3', '2000000003@est.umss.edu', 'Carlos', 'Martínez', NULL, '9876543210', 'https://randomuser.me/api/portraits/men/30.jpg'),
+  ('usuario4', '2000000004@est.umss.edu', 'Laura', 'Fernández', 'Sánchez', '9833543210', 'https://randomuser.me/api/portraits/women/30.jpg'),
+  ('usuario5', '2000000005@est.umss.edu', 'Pedro', 'Díaz', 'Gómez', '5555555555', 'https://randomuser.me/api/portraits/men/31.jpg'),
+  ('usuario6', '2000000006@est.umss.edu', 'Ana', 'Rodríguez', 'Hernández', '6666666666', 'https://randomuser.me/api/portraits/women/30.jpg'),
+  ('usuario7', '2000000007@est.umss.edu', 'Miguel', 'López', NULL, '7777777777', 'https://randomuser.me/api/portraits/men/2.jpg'),
+  ('usuario8', '2000000008@est.umss.edu', 'Sofía', 'Pérez', 'Martínez', '98765432131', 'https://randomuser.me/api/portraits/women/2.jpg'),
+  ('usuario9', '2000000009@est.umss.edu', 'Javier', 'Gómez', NULL, '8888888888', 'https://randomuser.me/api/portraits/men/10.jpg'),
+  ('usuario10', '2000000010@est.umss.edu', 'Elena', 'Sánchez', 'Martínez', '9999999999', 'https://randomuser.me/api/portraits/women/10.jpg');
 
+INSERT INTO CATEGORIA (nombre) VALUES
+  ('Matemáticas'),
+  ('Física'),
+  ('Programación'),
+  ('Programación Funcional'),
+  ('Estructuras de Datos'),
+  ('Algoritmos'),
+  ('Redes de Computadoras'),
+  ('Sistemas Operativos'),
+  ('Ingeniería de Software'),
+  ('Bases de Datos'),
+  ('Inteligencia Artificial'),
+  ('Seguridad Informática'),
+  ('Análisis y Diseño de Sistemas'),
+  ('Desarrollo Web'),
+  ('Desarrollo Móvil'),
+  ('Computación en la Nube'),
+  ('Arquitectura de Computadores'),
+  ('Sistemas Embebidos'),
+  ('Ingeniería de Requisitos'),
+  ('Interacción Humano-Computadora');
+
+INSERT INTO CONTENIDO (ID_usuario, descripcion) VALUES
+  (1, "## Criptografía y Teoría de Números\n\nLa criptografía moderna se basa en principios matemáticos sólidos, especialmente en la teoría de números. En este tema, discutiremos cómo se aplican los conceptos de teoría de números en el diseño de sistemas de cifrado y cómo estas técnicas protegen la información en el mundo digital.\n\n| **Contenido** | **Autor** | **Fecha de Publicación** |\n|---------------|-----------|--------------------------|\n| [Criptografía Post-Clásica: Aplicaciones y Desafíos](https://example.com/criptografia) | Juan Pérez | 2024-05-05 |\n| [Principios de RSA y su Aplicación en la Seguridad Informática](https://example.com/rsa) | María García | 2024-05-06 |\n\n![Imagen de Criptografía](https://isohub.org/wp-content/uploads/2021/10/criptografia_isohub.png)"),
+  (4, "## Física cuántica y sus aplicaciones en computación\n\bLa física cuántica está revolucionando el mundo de la computación. En este tema, exploraremos cómo los principios de la mecánica cuántica se aplican en el diseño de computadoras cuánticas y cómo estas pueden resolver problemas que son prácticamente imposibles de abordar para las computadoras clásicas.\n\n| **Contenido** | **Autor** | **Fecha de Publicación** |\n|---------------|-----------|--------------------------|\n| [Computación Cuántica: Del Laboratorio a la Realidad](https://example.com/computacion-cuantica) | Carlos Martínez | 2024-05-07 |\n| [Simulaciones Cuánticas y su Impacto en la Industria](https://example.com/simulaciones-cuanticas) | Laura Fernández | 2024-05-08 |\n\n![Imagen de Física Cuántica](https://planetario.montevideo.gub.uy/sites/planetario.montevideo.gub.uy/files/articulos/imagenes/fisica_cuantica_22.jpg)"),
+  (5, "## Introducción a la programación funcional con Haskell\n\nHaskell es un lenguaje de programación funcional puro que ha ganado popularidad en los últimos años. En este tema, aprenderemos los conceptos básicos de Haskell y cómo su enfoque funcional puede conducir a un código más claro y conciso.\n\n| **Contenido** | **Autor** | **Fecha de Publicación** |\n|---------------|-----------|--------------------------|\n| [Fundamentos de Haskell: Sintaxis y Tipos de Datos](https://example.com/fundamentos-haskell) | Pedro Díaz | 2024-05-09 |\n| [Programación Funcional en la Práctica: Ejemplos y Ejercicios](https://example.com/programacion-funcional) | Ana Rodríguez | 2024-05-10 |\n\n![Imagen de Haskell](https://example.com/haskell.jpg)"),
+  (7, "Comentario prueba, no tomar importancia!!!"),
+  (3, "Comentario prueba, no tomar importancia 2!!!"),
+  (1, "Comentario prueba, no tomar importancia 3!!!"),
+  (7, "Comentario prueba, no tomar importancia 4!!!"),
+  (2, "Comentario prueba, no tomar importancia 5!!!"),
+  (9, "Comentario prueba, no tomar importancia 6!!!");
+
+INSERT INTO TEMA (ID, titulo) VALUES
+  (1, "Un poco sobre ciptografia"),
+  (2, "¿Es realmente importante saber sobre la fisica cuantica?"),
+  (3, "¡Desarrollando nuevas habilidades, rompiendo tus fronteras!");
+
+INSERT INTO COMENTARIO (ID, ID_tema) VALUES
+  (4, 3),
+  (5, 3),
+  (6, 3),
+  (7, 3),
+  (8, 3),
+  (9, 3);
+
+INSERT INTO R_TEMA_CATEGORIA (ID_tema, ID_categoria) VALUES
+  (1, 1),
+  (1, 6),
+  (1, 11),
+  (1, 12),
+  (2, 2),
+  (2, 17),
+  (3, 3),
+  (3, 4),
+  (3, 5),
+  (3, 6);
+
+INSERT INTO VISUALIZACION (ID_tema, ID_usuario) VALUES
+  (1, 10),
+  (1, 7),
+  (2, 7),
+  (2, 1),
+  (1, 2),
+  (1, 2),
+  (3, 8),
+  (3, 9),
+  (3, 4),
+  (3, 8),
+  (3, 9),
+  (3, 4),
+  (3, 8),
+  (3, 9),
+  (3, 4),
+  (1, 7);
+
+
+
+-- ACTUALIZACIONES
+UPDATE TEMA SET activo = 0 WHERE ID = 2;
