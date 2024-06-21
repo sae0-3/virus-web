9bc6787490e46eb42c3f646d9c7f1f463792d39a
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-06-21 11:43:42 -0400

    impresion de credenciales por consola

--- backend/src/core/configs/db.js
+++ backend/src/core/configs/db.js
@@ -3,6 +3,14 @@
 import mysql from 'mysql2/promise'
 
 
+console.table({
+  host: process.env.DATABASE_HOST,
+  user: 'root',
+  password: process.env.MARIADB_ROOT_PASSWORD,
+  database: process.env.MARIADB_DATABASE
+})
+
+
 const conn = await mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: 'root',
